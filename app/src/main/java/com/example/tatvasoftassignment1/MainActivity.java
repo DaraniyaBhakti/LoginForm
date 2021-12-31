76f384905d453210261cba737009883320b7d03f
Bhakti Daraniya <180540107035@darshan.ac.in>
2021-12-31 17:49:29 +0530

    Assignment 1 with changes

--- app/src/main/java/com/example/tatvasoftassignment1/MainActivity.java
+++ app/src/main/java/com/example/tatvasoftassignment1/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     Button topViewBtn;
     Button centerViewBtn;
